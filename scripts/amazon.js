3e1387a9299ee7a52fdc992c01165832ea96f427
Sakshyam Luitel <sakshyam.luitel@gmail.com>
2025-05-10 16:43:10 +0545

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -122,8 +122,12 @@ products.forEach((product) =>{
             })
           }
           //console.log(cart);
-        
-        
+          let cartQuantity = 0;
+        cart.forEach((item) => {
+          cartQuantity += item.quantity;
+        });
+
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
       });
     });
 
