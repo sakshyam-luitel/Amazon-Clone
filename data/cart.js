ef6a43d87d016d4285dc544a0a1a2673cf79ee22
sakshyam-luitel <sakshyam.luitel@gmail.com>
2025-05-20 21:21:01 +0545

    Save Data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,15 @@
 export let cart = JSON.parse(localStorage.getItem('cart'))|| 
 [
-  // {
-  //   productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  //   quantity :2
-  // },
-  // {
-  //   productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-  //   quantity : 1
-  // }
+  {
+    productId :'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+    quantity :2,
+    deliveryOptionsId  : '1'
+  },
+  {
+    productId : '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+    quantity : 1,
+    deliveryOptionsId : '2'
+  }
 ];
 
 export function saveToStorage()
@@ -31,7 +33,8 @@ export function addToCart(productId)
           {
             cart.push({
               productId :productId,
-              quantity : Number(document.querySelector(`.js-product-quantity-selector-${productId}`).value) 
+              quantity : Number(document.querySelector(`.js-product-quantity-selector-${productId}`).value) ,
+              deliveryOptionsId : '1'
             })
           }
           console.log(cart);
