ffb6584b5b1b5b8a878932a8fcb2b47848005ef0
Sakshyam Luitel <sakshyam73@gmail.com>
2026-03-30 12:22:24 +0545

    configured settings to login with email instead of username and handled login logic

--- amazon_clone/amazon_clone/settings.py
+++ amazon_clone/amazon_clone/settings.py
@@ -165,6 +165,13 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION':True,
 }
 
-# ACCOUNT_AUTHENTICATION_METHOD = 'email'
-# ACCOUNT_EMAIL_REQUIRED = True
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend',
+]
+
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+ACCOUNT_EMAIL_REQUIRED = True
+ACCOUNT_UNIQUE_EMAIL = True
+ACCOUNT_USERNAME_REQUIRED = False
 # ACCOUNT_USERNAME_REQUIRED = False
