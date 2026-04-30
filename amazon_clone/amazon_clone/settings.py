96bc68b49deee711c64f25bd06467d17971b64e4
Sakshyam Luitel <sakshyam73@gmail.com>
2026-04-30 16:46:00 +0545

    secret key adjustments

--- amazon_clone/amazon_clone/settings.py
+++ amazon_clone/amazon_clone/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a)qdz9d9f0s!^lun)c4ra%_@y#p@y_bi$iwwudheb+n!@6lz)('
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
