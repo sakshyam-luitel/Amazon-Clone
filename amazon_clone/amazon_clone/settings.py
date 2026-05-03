f945accfcbea7c255c4f2f12e08920de93754333
Sakshyam Luitel <sakshyam73@gmail.com>
2026-05-04 00:19:20 +0545

    white noise middleware and static file storage added in settings:

--- amazon_clone/amazon_clone/settings.py
+++ amazon_clone/amazon_clone/settings.py
@@ -66,6 +66,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -150,6 +151,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR , 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # os.path.join(BASE_DIR , 'static/images')]
 
 # Default primary key field type
